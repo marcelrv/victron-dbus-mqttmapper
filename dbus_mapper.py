05d3a250431bdcf9b370232278bc4e221fc8ce16
Marcel Verpaalen <marcel@verpaalen.com>
2024-07-02 14:39:21 +0200

    Add reason codes to disconnect messages

--- dbus_mapper.py
+++ dbus_mapper.py
@@ -93,7 +93,7 @@ class P1Mapper:
             self.logger.info("Source broker Connection Established")
             self.connected = 1
         else:
-            self.logger.warning(f"Source MQTT broker bad connection Returned code={rc}")
+            self.logger.warning(f"Source MQTT broker bad connection Returned code={rc}: {rc_desciptions[rc]}")
             self.connected = 0
         client.subscribe(MQTT_TOPIC)
         client.publish(
@@ -106,16 +106,18 @@ class P1Mapper:
         if rc == 0:
             self.logger.info("Victron MQTT broker Connection Established")
         else:
-            self.logger.warning(f"Victron MQTT broker bad connection Returned code={rc}")
+            self.logger.warning(f"Victron MQTT broker bad connection Returned code={rc}: {rc_desciptions[rc]}")
 
     def on_disconnect(self, client, userdata, rc):
         if rc != 0:
             self.logger.warning("Unexpected source MQTT disconnection. Will auto-reconnect")
+            self.logger.warning(f"Disconnection code {rc}: {rc_desciptions[rc]}")
             self.connected = 0
 
     def on_disconnect_victron(self, client, userdata, rc):
         if rc != 0:
             self.logger.warning("Unexpected Victron MQTT disconnection. Will auto-reconnect")
+            self.logger.warning(f"Disconnection code {rc}: {rc_desciptions[rc]}")
 
     def on_message(self, client, userdata, message):
         self.logger.debug(f"Message topic: {message.topic}")
@@ -186,6 +188,54 @@ class P1Mapper:
         if rc[0] != 0:
             self.logger.warning(f"Error message # {self.index} during publish: {rc}")
         self.index += 1
+
+rc_desciptions = {
+    0: "Success",
+    0: "Normal disconnection",
+    0: "Granted QoS 0",
+    1: "Granted QoS 1",
+    2: "Granted QoS 2",
+    4: "Disconnect with Will Message",
+    16: "No matching subscribers",
+    17: "No subscription existed",
+    24: "Continue authentication",
+    25: "Re-authenticate",
+    128: "Unspecified error",
+    129: "Malformed Packet",
+    130: "Protocol Error",
+    131: "Implementation specific error",
+    132: "Unsupported Protocol Version",
+    133: "Client Identifier not valid",
+    134: "Bad User Name or Password",
+    135: "Not authorized",
+    136: "Server unavailable",
+    137: "Server busy",
+    138: "Banned",
+    139: "Server shutting down",
+    140: "Bad authentication method",
+    141: "Keep Alive timeout",
+    142: "Session taken over",
+    143: "Topic Filter invalid",
+    144: "Topic Name invalid",
+    145: "Packet Identifier in use",
+    146: "Packet Identifier not found",
+    147: "Receive Maximum exceeded",
+    148: "Topic Alias invalid",
+    149: "Packet too large",
+    150: "Message rate too high",
+    151: "Quota exceeded",
+    152: "Administrative action",
+    153: "Payload format invalid",
+    154: "Retain not supported",
+    155: "QoS not supported",
+    156: "Use another server",
+    157: "Server moved",
+    158: "Shared Subscriptions not supported",
+    159: "Connection rate exceeded",
+    160: "Maximum connect time",
+    161: "Subscription Identifiers not supported",
+    162: "Wildcard Subscriptions not supported",
+}
 
 
 if __name__ == "__main__":
